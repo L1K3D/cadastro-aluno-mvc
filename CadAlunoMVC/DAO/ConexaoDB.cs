03d18ba3ce08ee8615a423c2b9469212643fccc3
Heitor Ferreira <heitor.ferreira231203@gmail.com>
2024-09-03 20:37:10 -0300

    Primeira versao do codigo 03.09.2024

---
+++ CadAlunoMVC/DAO/ConexaoDB.cs
@@ -0,0 +1,15 @@
+﻿namespace CadAlunoMVC.DAO
+{
+    public static class ConexaoDB
+    {
+        public static SqlConnection GetConexao()
+        {
+            string strCon = "Data Source=LOCALHOST;Initial Catalog=AULADB;user id=sa; password=123456";
+            SqlConnection conexao = new SqlConnection(strCon);
+            conexao.Open();
+            return conexao;
+        }
+
+    }
+
+}
